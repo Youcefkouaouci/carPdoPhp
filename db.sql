428bfb3c6b55e725608e218321ec66458a6a19ac
unknown <youcefkouaouci@gmail.com>
2025-03-07 10:27:21 +0100

    setting up the working ENVI and create DB

---
+++ db.sql
@@ -0,0 +1,23 @@
+
+CREATE DATABASE carPdoPhp;
+
+USE carPdoPhp;
+
+CREATE TABLE car  (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    model VARCHAR(255) NOT NULL,
+    brand VARCHAR(255) NOT NULL,
+    horsePower INT NOT NULL,
+    image VARCHAR(255) NOT NULL
+);
+
+INSERT INTO car (model, brand, horsePower, image)
+VALUES ('Model S', 'Tesla', 670, 'modelS.jpg');
+INSERT INTO car (model, brand, horsePower, image)
+VALUES ('Civic', 'Honda', 158, 'civic.jpg');
+INSERT INTO car (model, brand, horsePower, image)
+VALUES ('Golf', 'Volkswagen', 150, 'golf.jpg');
+INSERT INTO car (model, brand, horsePower, image)
+VALUES ('Mustang', 'Ford', 450, 'mustang.jpg');
+INSERT INTO car (model, brand, horsePower, image)
+VALUES ('911 Carrera', 'Porsche', 379, 'carrera.jpg');
